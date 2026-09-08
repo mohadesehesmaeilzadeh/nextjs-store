405f5544e5baf4d12b78d1abedd31d8c1a0ca54f
mohadesehesmaeilzadeh <mohadesehesmaeilzadeh712@gmail.com>
2026-09-08 10:42:30 +0330

    add: add layout for the app

---
+++ src/app/layout.js
@@ -0,0 +1,25 @@
+import StyledComponentsRegistry from "../lib/registry";
+import Providers from "./providers";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export const metadata = {
+  title: "NextStore",
+  description: "A simple responsive online store built with Next.js.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <StyledComponentsRegistry>
+          <Providers>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+          </Providers>
+        </StyledComponentsRegistry>
+      </body>
+    </html>
+  );
+}
