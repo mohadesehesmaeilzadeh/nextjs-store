c670ef35dd5495d80ff1cbb447cf048c87de755d
mohadesehesmaeilzadeh <mohadesehesmaeilzadeh712@gmail.com>
2026-09-12 18:33:13 +0330

    add: aading page for card

---
+++ src/app/cart/page.js
@@ -0,0 +1,10 @@
+import CartPageContent from "../../components/cart/CartPageContent";
+
+export const metadata = {
+  title: "Cart | NextStore",
+  description: "Review your NextStore shopping cart.",
+};
+
+export default function CartPage() {
+  return <CartPageContent />;
+}
