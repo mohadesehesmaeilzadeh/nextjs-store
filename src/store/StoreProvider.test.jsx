e08a2e601747379320f1821c81f05c530faf659d
mohadesehesmaeilzadeh <mohadesehesmaeilzadeh712@gmail.com>
2026-09-24 11:36:40 +0330

    edit: update the store provider test

--- src/store/StoreProvider.test.jsx
+++ src/store/StoreProvider.test.jsx
@@ -2,8 +2,14 @@ import { act, screen, waitFor } from "@testing-library/react";
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { useDispatch, useSelector } from "react-redux";
-import { products } from "../data/products";
-import { addToCart, clearCart, selectCartCount } from "./slices/cartSlice";
+import { products } from "../test-utils/productFixtures";
+import {
+  addToCart,
+  clearCart,
+  increaseQuantity,
+  removeFromCart,
+  selectCartCount,
+} from "./slices/cartSlice";
 import { resetCheckout } from "./slices/checkoutSlice";
 import { store } from "./store";
 import StoreProvider from "./StoreProvider";
@@ -25,6 +31,18 @@ function CartProbe() {
       <span>Cart count: {cartCount}</span>
       <button type="button" onClick={() => dispatch(addToCart(products[0]))}>
         Add product
+      </button>
+      <button
+        type="button"
+        onClick={() => dispatch(increaseQuantity(products[0].id))}
+      >
+        Increase product
+      </button>
+      <button
+        type="button"
+        onClick={() => dispatch(removeFromCart(products[0].id))}
+      >
+        Remove product
       </button>
     </div>
   );
@@ -67,6 +85,32 @@ describe("StoreProvider", () => {
       expect(JSON.parse(window.localStorage.getItem("nextstore-cart"))).toEqual([
         cartItem,
       ]);
+    });
+  });
+
+  it("persists quantity changes and removals", async () => {
+    jest.useFakeTimers();
+    const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime });
+    window.localStorage.setItem("nextstore-cart", JSON.stringify([cartItem]));
+
+    renderWithStoreProvider();
+
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+
+    await user.click(screen.getByRole("button", { name: /increase product/i }));
+
+    await waitFor(() => {
+      expect(JSON.parse(window.localStorage.getItem("nextstore-cart"))).toEqual([
+        { ...cartItem, quantity: 2 },
+      ]);
+    });
+
+    await user.click(screen.getByRole("button", { name: /remove product/i }));
+
+    await waitFor(() => {
+      expect(JSON.parse(window.localStorage.getItem("nextstore-cart"))).toEqual([]);
     });
   });
 });
